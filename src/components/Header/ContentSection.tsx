fbab784c03d53de5f5ab9bdbcdfe90474f3823b3
Lenar Gumerov <lenargamgam@gmail.com>
2025-04-25 06:51:08 +0300

    feat: full email on print version

--- src/components/Header/ContentSection.tsx
+++ src/components/Header/ContentSection.tsx
@@ -44,17 +44,19 @@ const ContentSection = ({ name, title, email, links }: ContentSectionProps) => {
         </div>
         <h2 className="mb-8">{translatedTitle}</h2>
         <div className="text-lg">
-          <div className="flex items-center gap-6">
+          <div className="flex print:flex-col items-center print:items-start gap-6 print:gap-2">
             <div className="flex items-center relative">
               <button
-                className="inline-flex items-center bg-transparent border-0 p-0 hover:scale-125 transition-all duration-300"
+                className="inline-flex items-center bg-transparent border-0 p-0 hover:scale-125 transition-all duration-300 print:hidden"
                 onClick={copyEmailToClipboard}
                 title={email}
               >
                 <div className="text-text-primary">
                   <SocialIcon type="email" />
                 </div>
+                
               </button>
+              <span className="text-text-primary text-md print:inline-block hidden">{email}</span>
 
               <AnimatePresence>
                 {copyStatus && (
@@ -79,20 +81,22 @@ const ContentSection = ({ name, title, email, links }: ContentSectionProps) => {
 
 
 
-            {links.map((link) => (
-              <a
-                key={link.name}
-                className="inline-flex items-center hover:scale-125 transition-all duration-300"
-                href={link.url}
-                target="_blank"
-                rel="noopener noreferrer"
-                title={link.displayText}
-              >
-                <div className="text-text-primary">
-                  <SocialIcon type={link.icon} />
-                </div>
-              </a>
-            ))}
+            <div className="flex items-center gap-6">
+              {links.map((link) => (
+                <a
+                  key={link.name}
+                  className="inline-flex items-center hover:scale-125 transition-all duration-300"
+                  href={link.url}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  title={link.displayText}
+                >
+                  <div className="text-text-primary">
+                    <SocialIcon type={link.icon} />
+                  </div>
+                </a>
+              ))}
+            </div>
           </div>
         </div>
       </div>
