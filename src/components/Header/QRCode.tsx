6c76ca9fbc07d5541a79b718d7f896019b2a0aa7
Lenar Gumerov <lenargamgam@gmail.com>
2025-10-27 21:04:05 +0300

    fix: clickable qr-code for pdf version

--- src/components/Header/QRCode.tsx
+++ src/components/Header/QRCode.tsx
@@ -2,8 +2,8 @@ import { withBasePath } from '../../lib/utils';
 
 export default function QRCode() {
     return (
-        <div className="hidden print:flex justify-center items-center">
-            <img src={withBasePath('qr-code.png')} style={{ flexGrow: 1, maxWidth: '100px', maxHeight: '100px', borderRadius: '8px' }} alt="QR Code" />
-        </div>
+        <a className="hidden print:flex justify-center items-center" href="https://lenargum.github.io/CV/" target="_blank" rel="noopener noreferrer" title="Open CV in browser">
+            <img src={withBasePath('qr-code.png')} style={{ flexGrow: 1, maxWidth: '100px', maxHeight: '100px', borderRadius: '8px' }} alt="Open CV in browser" />
+        </a>
     )
 }
